94dac75341d294b3a2f2dc8bd9f26b6623803ee6
Cesar <tiendaorion2020@gmail.com>
2025-08-20 13:59:40 -0400

    V.3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // ✅ Ruta para escanear archivos HTML y TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
